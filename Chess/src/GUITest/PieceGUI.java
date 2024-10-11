3c576d383384af47aee35e2633ea5b03aac7a22e
SkereBerens <skereberens@gmail.com>
2024-10-11 17:29:04 +0200

    Add files via upload

--- Chess/src/GUITest/PieceGUI.java
+++ Chess/src/GUITest/PieceGUI.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -19,8 +21,9 @@ public class PieceGUI extends JPanel {
 	BufferedImage sprite;
 	JLabel spriteDisplay = new JLabel();
 	//
-	String[] sprites = {"/images/whiteKnight.png", "/images/whiteBishop.png", "/images/whiteRook.png", "/images/whiteQueen.png", "/images/whitePawn.png", "/images/whiteKing.png",
-						"/images/blackKnight.png", "/images/blackBishop.png", "/images/blackRook.png", "/images/blackQueen.png", "/images/blackPawn.png", "/images/blackKing.png"};
+	URL spriteURL;
+	String[] sprites = {"images/whiteKnight.png", "images/whiteBishop.png", "images/whiteRook.png", "images/whiteQueen.png", "images/whitePawn.png", "images/whiteKing.png",
+						"images/blackKnight.png", "images/blackBishop.png", "images/blackRook.png", "images/blackQueen.png", "images/blackPawn.png", "images/blackKing.png"};
 	
 	public void MoveTo(int position) {
 		this.position = position;
@@ -33,7 +36,8 @@ public class PieceGUI extends JPanel {
 	public void Promote(int pieceIndexPromotion) throws IOException {
 		if(pieceIndex == 4 || pieceIndex == 10) {
 			pieceIndex = pieceIndexPromotion;
-			sprite = ImageIO.read(getClass().getResource(sprites[pieceIndex]));
+			spriteURL = PieceGUI.class.getClassLoader().getResource(sprites[pieceIndex]);
+			sprite = ImageIO.read(spriteURL);
 			spriteDisplay.setIcon(new ImageIcon(sprite));
 		}
 	}
@@ -41,7 +45,8 @@ public class PieceGUI extends JPanel {
 	public PieceGUI(int startPosition, int pieceIndex) throws IOException{
 		//this.setPreferredSize(new Dimension(600, 400));
 		this.pieceIndex = pieceIndex;
-		sprite = ImageIO.read(getClass().getResource(sprites[this.pieceIndex]));
+		spriteURL = PieceGUI.class.getClassLoader().getResource(sprites[pieceIndex]);
+		sprite = ImageIO.read(spriteURL);
 		spriteDisplay.setIcon(new ImageIcon(sprite));
 		 add(spriteDisplay);
 		 position = startPosition;
